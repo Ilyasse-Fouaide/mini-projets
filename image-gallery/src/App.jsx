051db1bec9524777206ab580f44f4c5a25cae4f9
Ilyasse Fouaide <ilyasse.fouaide@gmail.com>
2023-09-19 10:57:15 +0100

    add images

--- image-gallery/src/App.jsx
+++ image-gallery/src/App.jsx
@@ -1,9 +1,25 @@
+const images = [
+  { src: "https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+  { src: "https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+  { src: "https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+  { src: "https://images.pexels.com/photos/1624438/pexels-photo-1624438.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+  { src: "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1600" },
+  { src: "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+  { src: "https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+  { src: "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+  { src: "https://images.pexels.com/photos/1433052/pexels-photo-1433052.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+]
+
 function App() {
 
   return (
-    <>
-      <h1 className="text-red-500">Starter Project</h1>
-    </>
+    <div className="max-w-7xl mx-auto columns-2 p-5 gap-5 space-y-5">
+      {images.map((image, index) =>
+        <div>
+          <img src={image.src} />
+        </div>
+      )}
+    </div>
   )
 }
 
