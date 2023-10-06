626c94cbabb7b05f2db5a6c797d6f44292b3a4d8
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-10-06 19:19:30 +0100

    setup nav section

--- loop-studio/src/App.jsx
+++ loop-studio/src/App.jsx
@@ -1,9 +1,21 @@
+import heroBg from "./images/desktop/image-hero.jpg";
+
 function App() {
 
   return (
-    <>
-      <h1 className="text-red-500">Starter Project</h1>
-    </>
+    <div className={`container max-w-6xl mx-auto px-6 py-12 lg:px-0 bg-[url('./images/desktop/image-hero.jpg')]`}>
+      <nav className="flex justify-between items-center text-white font-bold">
+        <div><a href="#">Meta<span className="text-blue-500">VERSE</span></a></div>
+        <div className="hidden md:flex md:space-x-10">
+          <div className="group">
+            <a href="#" className="relative">
+              Home
+              <div className="absolute w-0 left-1/2 -translate-x-1/2 h-[2px] bg-blue-500 group-hover:w-full duration-200"></div>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
