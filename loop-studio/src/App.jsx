68825fe50b067748c46e2a9858bcd7713d7947e2
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-10-06 20:04:46 +0100

    section 1 done

--- loop-studio/src/App.jsx
+++ loop-studio/src/App.jsx
@@ -3,18 +3,27 @@ import heroBg from "./images/desktop/image-hero.jpg";
 function App() {
 
   return (
-    <div className={`container max-w-6xl mx-auto px-6 py-12 lg:px-0 bg-[url('./images/desktop/image-hero.jpg')]`}>
-      <nav className="flex justify-between items-center text-white font-bold">
-        <div><a href="#">Meta<span className="text-blue-500">VERSE</span></a></div>
-        <div className="hidden md:flex md:space-x-10">
-          <div className="group">
-            <a href="#" className="relative">
-              Home
-              <div className="absolute w-0 left-1/2 -translate-x-1/2 h-[2px] bg-blue-500 group-hover:w-full duration-200"></div>
-            </a>
+    <div className="bg-[url('./images/desktop/image-hero.jpg')] bg-no-repeat bg-cover bg-center">
+
+      <div className={`container max-w-6xl mx-auto px-6 py-12`}>
+        <nav className="flex justify-between items-center text-white font-bold">
+          <div className="text-2xl"><a href="#">Meta<span className="text-blue-500 text-3xl">VERSE</span></a></div>
+          <div className="hidden md:flex md:space-x-5">
+            {['About', 'Carrer', 'Events', 'Products', 'Support'].map((items, index) =>
+              <div className="px-2 py-1">
+                <a href="#" className="relative group">
+                  {items}
+                  <div className="absolute w-0 left-1/2 -translate-x-1/2 h-[3px] bg-white group-hover:w-full duration-200"></div>
+                </a>
+              </div>
+            )}
           </div>
+        </nav>
+        {/* // TODO Hamburgur button */}
+        <div className="max-w-xl my-32 border-2 p-4 rounded-md text-left uppercase text-white text-6xl font-light md:p-10">
+          Impressive Exepriences That Deliver
         </div>
-      </nav>
+      </div>
     </div>
   )
 }
