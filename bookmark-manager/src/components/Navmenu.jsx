889e71d8d9b20226418c4e88d4e87698160520c6
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-10-11 19:46:12 +0100

    fixe nav menu issue in desktop version

--- bookmark-manager/src/components/Navmenu.jsx
+++ bookmark-manager/src/components/Navmenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navmenu({ isClicked }) {
   return (
-    <div className={`z-10 fixed top-0 w-full h-[100vh] transition-opacity duration-200 ${isClicked ? '' : 'hidden'} bg-indigo-950 divide-y-2 divide-gray-300 pt-24`}>
+    <div className={`md:hidden z-10 fixed top-0 w-full h-[100vh] transition-opacity duration-200 ${isClicked ? '' : 'hidden'} bg-indigo-950 divide-y-2 divide-gray-300 pt-24`}>
       {['Features', 'Download', 'Faq'].map((item, index) => (
         <div key={index}>
           <div className='flex flex-col justify-center items-center py-2 text-gray-200 uppercase'>
